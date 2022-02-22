4c2fb86150001158de39936c6c7b25355a347ca0
Mikhail Grishin <wopolebereza@gmail.com>
2022-02-22 15:01:03 +0300

    counter

---
+++ counter/src/App.js
@@ -0,0 +1,37 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [item, setItem] = useState(0);
+  const incrementItem = () => setItem(item + 1);
+  const decrementItem = () => setItem(item - 1);
+  const clearItem = () => setItem(0);
+  
+  return (
+    <div className='wrapper'>
+      <div className='wrapper-second'>
+        <button className='btn btn-up' onClick={incrementItem}><span>&#9650;</span> Go up <span>&#9650;</span></button>
+        <div className='App'>
+          <h1>{item}</h1>
+        </div>
+        <button className='btn btn-down' onClick={decrementItem}><span>&#9660;</span> Go down <span>&#9660;</span></button>
+      </div>
+      <div className='cleaning'><button className='btn btn-clear' onClick={clearItem}>Clear</button></div>
+    </div>
+  )
+}
+
+// function Timer() {
+
+//     return (
+//       <div className='timer'>
+//         <div className='timer'></div>
+//         <div className='container'></div>
+//         <div className='timer-container'></div>
+//         <h1>Timer</h1>
+//       </div>
+//     )
+// }
+
+export default App;
+// export Timer;
