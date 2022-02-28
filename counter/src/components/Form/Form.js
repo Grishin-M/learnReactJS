9010335e723899a29cfe80502ba555701fd0e523
Mikhail Grishin <wopolebereza@gmail.com>
2022-02-28 12:45:56 +0300

    refactor: merge some  useStates into one useState

--- counter/src/components/Form/Form.js
+++ counter/src/components/Form/Form.js
@@ -1,12 +1,19 @@
 import { useState } from 'react';
 import '../Form/Form.css'
 
+// один useState ???? { } refactoring
+// один обработчик
+
 function From() {
-  const [input1, setInput1] = useState('');
-  const [input2, setInput2] = useState('');
-  const [input3, setInput3] = useState();
-  const [result, getResult] = useState();
-  
+  const StartValues = {
+    Name: '',
+    Surname: '',
+    Age: '',
+    Result: ''
+  }
+
+  const [input, setInput] = useState(StartValues);
+
   return (
       <div className='wrapper-form'>
        <form className='form'>
@@ -15,14 +22,38 @@ function From() {
             <label className='form-label'>Фамилия<sup>*</sup>: </label>
             <label className='form-label'>Возраст: </label>
           </div>
+
           <div className='form-style'>
-            <input className='form-input' type='text' placeholder='Иван' required value={input1} onChange={event => setInput1(event.target.value)}></input>
-            <input className='form-input' type='text' placeholder='Иванов' required value={input2} onChange={event => setInput2(event.target.value)}></input>
-            <input className='form-input' type='number' value={input3} onChange={event => setInput3(event.target.value)}></input>
+           
+            <input className='form-input' type='text' placeholder='Иван' required
+            onChange={(event) => setInput((prevState) => ({
+              ...prevState,
+              Name: event.target.value
+            }))}></input>
+           
+            <input className='form-input' type='text' placeholder='Иванов' required 
+            onChange={(event) => setInput((prevState) => ({
+              ...prevState,
+              Surname: event.target.value
+            }))}></input>
+           
+            <input className='form-input' type='number' 
+            onChange={(event) => setInput((prevState) => ({
+              ...prevState,
+              Age: event.target.value
+            }))}></input>
+
           </div>
-          <div className='form-btn'><button className='btn-form' type="button" onClick={() => getResult(String('Имя: ' + input1) + ' ' + String('Фамилия: ' + input2) + ' ' + String('Возраст: ' + input3))}>Отправить</button>
-          <p>Результат: {result}</p>
+
+          <div className='form-btn'>    
+            <button className='btn-form' type="button" onClick={() => setInput(() => ({
+              Result: String('Имя: ' + input.Name) + ' ' + 
+              String('Фамилия: ' + input.Surname) + ' ' + 
+              String('Возраст: ' + input.Age)
+              }))}>Отправить</button>
+            <p>Результат: {input.Result}</p>
           </div>
+
        </form>
       </div>
   )
