cca9c3a69c6584953ca2a69f1cac66c339cce42c
Mikhail Grishin <wopolebereza@gmail.com>
2022-03-03 12:08:49 +0300

    some changes into memo

--- counter/src/components/useMemo/counterForMemo.js
+++ counter/src/components/useMemo/counterForMemo.js
@@ -7,19 +7,14 @@ import Count from './Count';
 function CounterForMemo() {
 
   const [count1, setCount1] = useState(0);
-  const [count2, setCount2] = useState(0);
-
+  
   return (
     <div className='wrapCounter'>
-      <h5>Counter 1</h5>
+      <h5>Counter with useMemo</h5>
       <div className='counter'>
         <button onClick={() => setCount1(count1 + 1)}>+</button>
+        <button onClick={() => setCount1(count1 - 1)}>-</button>
         <Count value={count1} />
-      </div>
-      <h5>Counter 2</h5>
-      <div className='counter'>
-        <button onClick={() => setCount2(count2 + 1)}>+</button>
-        <Count value={count2} />
       </div>
     </div>
   );
