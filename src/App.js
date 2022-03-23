2f189c51645130c5759be4f624014257b2798217
Гришин Михаил <mikhail.v.grishin@gazprombank.ru>
2022-03-23 13:19:23 +0300

    done task 16147

--- src/App.js
+++ src/App.js
@@ -1,43 +1,34 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom'
 import Form from './15140/Form/Form'
 import Timer from './15140/Timer/Timer'
 import MyApiComponent from './15395/Api/Api'
 import AnyFunc from './15466/useCallback/AnyFunc'
 import CounterForMemo from './15466/useMemo/counterForMemo'
 import Ref from './15466/useRef/Ref'
-import Input from './15911/Input'
-import TodoItem from './15911/TodoItem'
-
-import { useSelector } from 'react-redux'
-import { selectTodoList } from './features/todoSlice'
-
+import MyTodo from './15911/MyTodo'
 
 function App() {
-  const todoList = useSelector(selectTodoList)
-
   return (
     <div className='wrapper'>
-      <div className='wrapp_container'>
-        <div className='wrapp_todoContainer'>
-          {
-            todoList.map(item => (
-              <TodoItem
-                name={item.item}
-                done={item.done}
-                id={item.id}
-                key={item.toString()}
-              />
-            ))
-          }
-        </div>
-        <Input />
-      </div>
-      <Ref />
-      <CounterForMemo />
-      <AnyFunc />
-      <Form />
-      <Timer />
-      <MyApiComponent />
+      <ul className='links'>
+        <ol><a href='/ref'>Ref</a></ol>
+        <ol><a href='/counterForMemo'>CounterForMemo</a></ol>
+        <ol><a href='/anyFunc'>AnyFunc</a></ol>
+        <ol><a href='/form'>Form</a></ol>
+        <ol><a href='/timer'>Timer</a></ol>
+        <ol><a href='/myApiComponent'>MyApiComponent</a></ol>
+        <ol><a href='/todo'>Todo</a></ol>
+      </ul>
+      <Routes>
+        <Route path='/ref' element={<Ref />}/>
+        <Route path='/counterForMemo' element={<CounterForMemo />}/>
+        <Route path='/anyFunc' element={<AnyFunc />}/>
+        <Route path='/form' element={<Form />}/>
+        <Route path='/timer' element={<Timer />}/>
+        <Route path='/myApiComponent' element={<MyApiComponent />}/>
+        <Route path='/todo' element={<MyTodo />}/>
+      </Routes>
     </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom'
 import './index.css';
 import { Provider } from 'react-redux';
 import App from './App';
@@ -8,8 +9,10 @@ import configureStore from './app/store';
 const store = configureStore
 
 ReactDOM.render(
-    <Provider store={store}>
-      <App />
-    </Provider>,
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>,
   document.getElementById('root'),
 );
